69534dc5b9ad8408576ea6a3b91f2e21d758ba93
Ben Booth <bkbooth@gmail.com>
2019-03-14 16:35:32 +1100

    Fix global graphql tag warning

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import format from 'date-fns/format';
 import Meta from '../components/meta';
