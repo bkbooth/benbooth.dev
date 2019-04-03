3728ce91214aefcf7ac37b316b5a8bd3aea0670b
Ben Booth <bkbooth@gmail.com>
2019-04-04 06:18:06 +1100

    Fix FontAwesome icons flashing huge by preloading CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,10 @@
 import 'typeface-merriweather';
 import 'typeface-open-sans';
 
+// Preload FontAwesome CSS
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 // Prism.js theme
 import 'prism-themes/themes/prism-ghcolors.css';
